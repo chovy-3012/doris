9f467662d3d0d4c24da2059aa76e67c6b5982e19
wei.zhao <vergilchiu@foxmail.com>
2020-12-08 10:15:47 +0800

    modify build script

--- my-script/mybuild.sh
+++ my-script/mybuild.sh
@@ -1,5 +1,12 @@
 dir=$(dirname $(readlink -f $0))
+
+echo "build fe and be"
 cd $dir/../
 sh ./build.sh
-cd fs_brokers/apache_hdfs_broker
+
+echo "build broker"
+cd $dir/../fs_brokers/apache_hdfs_broker
 sh build.sh
+
+cd $dir/../
+chmod -R 777  output
--- my-script/package.sh
+++ my-script/package.sh
@@ -3,15 +3,19 @@ dir=$(dirname $(readlink -f $0))
 cd $dir/../
 rm -fr doris
 mkdir -p doris
-echo "######package fe####"
-cp -r output/* doris
+
+echo "######package fe ####"
+cp -r output/fe doris
+cp -r output/udf doris
 cp -r webroot/* doris/fe/webroot
 mkdir -p doris/fe/log
 mkdir -p doris/fe/doris-meta
+
 echo "######package be####"
 cp -r be/output/ doris/be
 mkdir -p doris/be/log
 mkdir -p doris/be/storage
+
 echo "######package broker####"
 cp -r fs_brokers/apache_hdfs_broker/output/apache_hdfs_broker doris/broker
 mkdir -p doris/broker/log
